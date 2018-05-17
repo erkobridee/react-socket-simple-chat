f5baf2318a92066a9d8d8a6b21f00582d7957062
Erko Bridee <erko.bridee@gmail.com>
2018-05-17 03:43:05 -0300

    add basic usage of react-router-dom

--- src/chat/index.js
+++ src/chat/index.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
 
+import {
+  BrowserRouter as Router, Route, Link
+} from 'react-router-dom';
+
 import styles from './styles.scss';
+
+//----------------------------------------------------------------------------//
+
+const ChatRoom = () => (
+  <div>TODO: define chat room component</div>
+);
+
+const Settings = () => (
+  <div>TODO: define settings component</div>
+);
+
+const NavBar = () => (
+  <div>
+    <Link to="/">Chat</Link> | 
+    <Link to="/settings">Settings</Link>
+  </div>
+);
+
+const Routes = () => (
+  <div>
+    <Route exact path="/" component={ChatRoom} />
+    <Route path="/settings" component={Settings} />
+  </div>
+);
+
+//----------------------------------------------------------------------------//
 
 class Chat extends Component {
 
   render(){
 
     return (
-      <div className={styles.chat}>
-        <h1>React webapp build with the Webpack v4.</h1>
-        <p><strong>.env message:</strong> {process.env.MESSAGE}</p>
-      </div>
+      <Router>
+        <div className={styles.chat}>
+          <h1>React webapp build with the Webpack v4.</h1>
+          <p><strong>.env message:</strong> {process.env.MESSAGE}</p>
+          <br/>
+          <NavBar />
+          <hr />
+          <Routes />
+        </div>
+      </Router>
     );
   }
 }
