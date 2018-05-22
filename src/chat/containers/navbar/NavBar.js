ba9d4a7619e0d669b279866a7dcea1c3df6764b2
Erko Bridee <erko.bridee@gmail.com>
2018-05-22 01:43:53 -0300

    add a comment about the logic flow to implement to display the unreaded

--- src/chat/containers/navbar/NavBar.js
+++ src/chat/containers/navbar/NavBar.js
@@ -6,8 +6,30 @@ import classNames from 'classnames';
 
 class NavBar extends Component {
 
+  state = {
+    isSettings: false,
+    messagesLenghtSnapshot: 0,
+    messagesUnreaded: 0
+  }
+
+  // TODO: use a local state to check if the user is on the settings page
+  /*
+      access the messages and read how many are there before
+      the user swith to the settings page, and display the
+      count of messages received unreaded to diplay
+
+      1. onClick over settings tab
+        1. set a flag say it's on the settings tab
+        2. access the messages state and take a "snapshot" of the messages length
+        3. update the counter with the last messages lenght minus the messagaeLenghtSnapshot
+      2. onClick over chat tab
+        1. reset the flag
+        2. reset the messagesLenghtSnapshot
+        3. reset the counter
+  */
+
   render() {
-    const theme = this.props.theme || 'light'; 
+    const theme = this.props.theme || 'light';
 
     const navbarClass = classNames(
       'navbar',
