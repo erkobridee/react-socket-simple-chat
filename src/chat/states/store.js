c3a681e8ac162d6a325afaafadeced59fac0e7bf
Erko Bridee <erko.bridee@gmail.com>
2018-05-24 00:39:29 -0300

    add some fixes to the code and support to hot reload the redux reducers

--- src/chat/states/store.js
+++ src/chat/states/store.js
@@ -6,14 +6,20 @@ import LocalStorageService from 'chat/services/localstorage';
 import MessagesUtils from './ducks/messages/utils';
 import combinedReducers from './ducks';
 
+//----------------------------------------------------------------------------//
 
 // https://stackoverflow.com/questions/37876889/react-redux-and-websockets-with-socket-io
 const socketClient = {
   todo: 'define the socket.io API',
-  on: () => {}
+  on: () => {},
+  emit: (event, payload) => {
+    console.log(`socket event: ${event} with the payload: `, payload);
+  }
 };
 
 const persistedState = LocalStorageService.loadState();
+
+//----------------------------------------------------------------------------//
 
 const store = createStore(
   combinedReducers,
@@ -23,14 +29,28 @@ const store = createStore(
   )
 );
 
+// hack to reload the reducer
+// IMPORTANT: that won't work to this file, and you'll get an error on the browser console
+// https://github.com/reduxjs/react-redux/releases/tag/v2.0.0
+if( module.hot ){
+  module.hot.accept('./ducks', () => {
+    const nextRootReducer = require(`./ducks/index`);
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
+//----------------------------------------------------------------------------//
+
 MessagesUtils.listenSocketEvents( socketClient, store.dispatch );
 
 LocalStorageService.subscribe(
   store.subscribe,
-  // defines which data will be persisted
-  {
+  // defines a callback to get the data which will be persisted
+  () => ({
     messages: store.getState().messages
-  }
+  })
 );
+
+//----------------------------------------------------------------------------//
 
 export default store;
