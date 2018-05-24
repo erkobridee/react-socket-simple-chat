4421c5ca456b2086f789c04942a8304ecec32fa0
Erko Bridee <erko.bridee@gmail.com>
2018-05-24 00:40:25 -0300

    fix subscribe function

--- src/chat/services/localstorage/utils.js
+++ src/chat/services/localstorage/utils.js
@@ -10,11 +10,11 @@ const THROTTLE_DELAY = 1000; // ms - 1s
   receives the redux store.subscribe and the giver date from its state to
   persist in the web browser localStorage
 */
-export const subscribe = ( subscribe, data ) => {
-  if( subscribe && data ) {
+export const subscribe = ( subscribe, getData ) => {
+  if( subscribe && getData ) {
     subscribe(
       throttle(
-        () => saveState( data ) , THROTTLE_DELAY
+        () => saveState( getData() ) , THROTTLE_DELAY
       )
     );
   }
