cdf22d16c97bf2a4013f6b9a44ebb2fec92fc063
Erko Bridee <erko.bridee@gmail.com>
2018-05-26 01:37:41 -0300

    SafeNavLink move to stateless component

--- src/chat/components/navlink/SafeNavLink.js
+++ src/chat/components/navlink/SafeNavLink.js
@@ -1,37 +1,61 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import { NavLink, withRouter } from 'react-router-dom';
 import classNames from 'classnames';
 
-// solves the error message
-// Warning: Hash history cannot PUSH the same path; a new entry will not be added to the history stack
+/*
+  redefines a new NavLink component to solves the error message:
+    Warning: Hash history cannot PUSH the same path;
+    a new entry will not be added to the history stack
 
-class SafeNavLink extends Component {
- 
-  render() {
-    const { 
-      children, 
-      history, to, 
-      className, activeClassName, 
-      staticContext, ...rest 
-    } = this.props;
-    const isActive = (history.location.pathname === to);
-    const applyActiveCss = isActive ? (activeClassName || 'active') : null;
-    const applyActiveStyle = {
-      cursor: 'default'
-    };
-    const applyCss = classNames(
-      className, applyActiveCss
-    );
-    return (
-      <Fragment>
-        {
-          isActive ?
-            <a className={ applyCss } style={ applyActiveStyle }>{ children }</a> :
-            <NavLink className={applyCss} { ...{ to, ...rest } }>{ children }</NavLink>
-        }
-      </Fragment>
-    );
-  }
-}
+  same usage to the <NavLink>:
 
+  <SafeNavLink></SafeNavLink>
+
+  reference:
+    https://reacttraining.com/react-router/web/api/NavLink
+*/
+export const SafeNavLink = ({
+  children,
+  history, to,
+  className, activeClassName,
+  staticContext,
+  ...rest
+}) => {
+
+  const isActive = ( history.location.pathname === to );
+  const applyActiveCss = isActive ? ( activeClassName || 'active' ) : null;
+
+  const applyActiveStyle = {
+    cursor: 'default'
+  };
+
+  const applyCss = classNames(
+    className, applyActiveCss
+  );
+
+  return (
+    <Fragment>
+      {
+        isActive ?
+          <a className={ applyCss } style={ applyActiveStyle }>{ children }</a> :
+          <NavLink className={applyCss} { ...{ to, ...rest } }>{ children }</NavLink>
+      }
+    </Fragment>
+  );
+};
+
+// https://reactjs.org/docs/typechecking-with-proptypes.html
+SafeNavLink.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.element,
+    PropTypes.arrayOf(PropTypes.element)
+  ]).isRequired,
+  history: PropTypes.object.isRequired,
+  to: PropTypes.string.isRequired
+};
+
+// the withRouter wrapper function will provide the same attributes that
+// the NavLink has access by default
 export default withRouter(SafeNavLink);
