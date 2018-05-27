d6d9bfe5a838fc5a2f6d16ef45ce02973820d3ad
Erko Bridee <erko.bridee@gmail.com>
2018-05-26 23:47:17 -0300

    define an i18n service

--- src/chat/services/index.js
+++ src/chat/services/index.js
@@ -1,7 +1,21 @@
 import hash from './hash';
+import i18n from './i18n';
 import checkIsMobile from './is-mobile';
+import localstorage from './localstorage';
+import socketclient from './socketclient';
 
 export {
   hash,
-  checkIsMobile
+  i18n,
+  checkIsMobile,
+  localstorage,
+  socketclient
+};
+
+export default {
+  hash,
+  i18n,
+  checkIsMobile,
+  localstorage,
+  socketclient
 };
